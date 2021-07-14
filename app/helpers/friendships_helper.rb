e9a9743e26390824ddf65e77a24994e6617d9cbd
Maha Magdy <mahamagdy3@gmail.com>
2021-07-15 00:09:55 +0200

    Hide the friendship invitation from the current user to himself/herself

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,6 @@
 module FriendshipsHelper
+    def show_friendship(user)
+        return if current_user[:id] == user[:id]
+        link_to('Send Friend Request',  friendships_path(receiver_id: user[:id]), method: :post, class: 'profile-link')
+    end
 end
