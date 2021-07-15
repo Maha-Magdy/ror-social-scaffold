d5630ea95bef44d9442f414ef66fb532895065f0
Maha-Magdy <mahamagdy3@gmail.com>
2021-07-15 22:16:11 +0200

    Build the feature of friendship to be able to send and recieve ones

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,4 +8,24 @@ class FriendshipsController < ApplicationController
             redirect_to users_path, alert: 'Your friend request cannot been sent.'
         end
     end
+
+    def destroy
+        friendship = Friendship.find_by(id: params[:id], sender_id:  params[:sender_id], receiver_id: params[:receiver_id])
+        if friendship
+            friendship.destroy
+            redirect_to users_path, notice: 'Friendship request has been canceled.'
+        else
+            redirect_to users_path, alert: 'Friendship request cannot been canceled.'
+        end
+    end
+
+    def update
+        friendship = Friendship.find_by(id: params[:id], sender_id: params[:sender_id], receiver_id: current_user)
+        friendship.confirmed = true
+        if friendship.save
+            redirect_to users_path, notice: "#{User.find_by(id: friendship.sender_id).name} become your friend now"
+        else
+            redirect_to users_path, alert: 'Something went wrong, and you cannot accept the invitation now'
+        end
+    end
 end
--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,6 +1,20 @@
 module FriendshipsHelper
-    def show_friendship(user)
-        return if current_user[:id] == user[:id]
-        link_to('Send Friend Request',  friendships_path(receiver_id: user[:id]), method: :post, class: 'profile-link')
+  def show_friendship(user)
+    return if current_user[:id] == user[:id]
+
+    send_friendship = Friendship.find_by(sender_id: current_user[:id], receiver_id: user[:id])
+    pending_friendship = Friendship.find_by(sender_id: user[:id], receiver_id: current_user[:id], confirmed: false)
+    accepted_friendship = Friendship.find_by(sender_id: user[:id], receiver_id: current_user[:id], confirmed: true)
+
+    if send_friendship
+      link_to('Cancel Friendship Request',  friendship_path(id: send_friendship[:id], sender_id: current_user[:id], receiver_id: user[:id]), method: :DELETE, class: 'btn-link')
+    elsif pending_friendship
+      link_to('Accept Friendship Request',  friendship_path(id: pending_friendship[:id], sender_id: user[:id], receiver_id: current_user[:id]), method: :PUT, class: 'btn-link') +
+      link_to('Remove Friendship Request',  friendship_path(id: pending_friendship[:id], sender_id: user[:id], receiver_id: current_user[:id]), method: :DELETE, class: 'btn-link')
+    elsif accepted_friendship
+      link_to('Remove Friendship',  friendship_path(id: accepted_friendship[:id], sender_id: user[:id], receiver_id: current_user[:id]), method: :DELETE, class: 'btn-link')
+    else
+      link_to('Send Friend Request',  friendships_path(receiver_id: user[:id]), method: :post, class: 'btn-link')
     end
+  end
 end
